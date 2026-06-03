5d2b5ca57ee3b6cc5171fb7899b1ab4123b23ce7
Dev-MuradAbed <93073520+Dev-MuradAbed@users.noreply.github.com>
2026-06-03 23:08:18 +0300

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export PUPPETEER_CACHE_DIR=/opt/render/project/src/.cache/puppeteer
-
-echo "==> تثبيت Chrome..."
-npx puppeteer browsers install chrome --path $PUPPETEER_CACHE_DIR
-echo "==> Chrome جاهز ✅"
-
-echo "==> تشغيل البوت..."
-node index.js
